9360a6906776928c5add437b8846a102357f3485
kanson5168 <1174195268@qq.com>
2024-06-29 13:55:14 +0800

    项目搭建

---
+++ kanson-react-system/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>搭建顶级组件</div>
+    </>
+  )
+}
+
+export default App
---
+++ kanson-react-system/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// 样式初始化一般放前面
+import 'normalize.css'
+// ui框架的样式
+
+// 全局样式
+import "./assets/styles/global.scss"
+// 组件的样式
+import App from './App.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
